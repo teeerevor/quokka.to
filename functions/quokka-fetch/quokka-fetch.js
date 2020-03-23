3133aa6c886842a8051270ad501957fee1819adc
Trev <teeerevor@gmail.com>
2020-03-24 00:38:12 +1100

    moar quokkas

--- functions/quokka-fetch/quokka-fetch.js
+++ functions/quokka-fetch/quokka-fetch.js
@@ -21,6 +21,12 @@ const QUOKKA_MAP = {
         maxHeight: 2376,
         url: 'https://commons.wikimedia.org/wiki/File:Quokka_family.jpg',
     },
+    esme: {
+        src: 'v1582292199/Quokka_Rottnest_Island_uhcdo9',
+        maxWidth: 1789,
+        maxHeight: 2386,
+        url: 'https://commons.wikimedia.org/wiki/Category:Setonix_brachyurus#/media/File:Quokka_Rottnest_Island.jpg',
+    },
     finn: {
         src: 'v1583062810/8670484575_42befbd867_o_c2dowi',
         maxWidth: 1200,
@@ -28,17 +34,48 @@ const QUOKKA_MAP = {
         url: 'https://www.flickr.com/photos/fraggy/8670484575/',
         portrait: true,
     },
+    gregor: {
+        src: 'v1582292199/Quokka_at_rottnest_cibkxs',
+        maxWidth: 1166,
+        maxHeight: 778,
+        url: 'https://commons.wikimedia.org/wiki/Category:Setonix_brachyurus#/media/File:Quokka_at_rottnest.jpg',
+    },
+    harry: {
+        src: 'v1584968004/Quokka_q7yvpm.jpg',
+        maxWidth: 1704,
+        maxHeight: 2272,
+        url: 'https://upload.wikimedia.org/wikipedia/commons/e/e0/Quokka.jpg',
+    },
+    ivy: {
+        src: 'v1584969287/5661728684332708_ibefii',
+        maxWidth: 1536,
+        maxHeight: 1542,
+        url:
+            'https://www.watson.ch/u/zoom?url=%2Fimgdb%2Fd211%2FQx%2CB%2C0%2C0%2C721%2C724%2C300%2C301%2C120%2C120%2F5661728684332708&source=Bild%3A%20Imgur&caption=',
+    },
+    jack: {
+        src: 'v1584969822/Quokka_selfie_vuwvwu.jpg',
+        maxWidth: 1280,
+        maxHeight: 960,
+        url: 'https://commons.wikimedia.org/wiki/Category:Setonix_brachyurus#/media/File:Quokka_selfie.jpg',
+    },
+    katie: {
+        src: 'v1584970287/RottnestQuokka_enh0ty',
+        maxWidth: 3625,
+        maxHeight: 2412,
+        url: 'https://commons.wikimedia.org/wiki/Category:Setonix_brachyurus#/media/File:RottnestQuokka.jpg',
+    },
     simon: {
         src: 'v1583062812/46680407785_53d6b13bd4_o_vhh79s',
         maxWidth: 1280,
         maxHeight: 960,
         url: 'https://www.flickr.com/photos/39551170@N02/46680407785',
     },
-    simon: {
-        src: 'v1582292199/Quokka_Rottnest_Island_uhcdo9',
-        maxWidth: 1280,
-        maxHeight: 960,
-        url: 'https://www.flickr.com/photos/39551170@N02/46680407785',
+    thomas: {
+        src: 'v1584906437/90170220_201f5a8d24_o_u5n1jn',
+        maxWidth: 1024,
+        maxHeight: 934,
+        url: 'https://www.flickr.com/photos/done/90170220',
     },
 };
 
